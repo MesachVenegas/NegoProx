e037582dd7ca164a0d4a3e4f86f6f05968c444a4
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-18 11:28:06 -0800

    feature: connect DB to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+
+import { PrismaModule } from '@presentation/modules/prisma.module';
+import { UserModule } from '@presentation/modules/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    UserModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
