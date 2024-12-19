3c4a3f24e0a70ed5101139b07ac51f57957801ff
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-19 15:24:55 -0800

    add: MailService , and usecases to User Module

--- backend/src/presentation/modules/user.module.ts
+++ backend/src/presentation/modules/user.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from '@infrastructure/services/user/user.service';
 import { UserController } from '@presentation/controllers/user/user.controller';
+import { VerifyUserUseCase } from '@app/use-cases/user/verify-email.use-case';
+import { UserRepository } from '@infrastructure/repositories/user.repository';
+import { JwtService } from '@nestjs/jwt';
+import { MailModule } from './mail.module';
 
 @Module({
+  imports: [MailModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserRepository, VerifyUserUseCase, JwtService],
   exports: [UserService],
 })
 export class UserModule {}
