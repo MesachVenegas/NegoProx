8a0a5b778150ba707968579ebd1015f67858395e
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-19 15:21:37 -0800

    add: token generator and send email to verify new accounts by email

--- backend/src/application/use-cases/user/register-user.use-case.ts
+++ backend/src/application/use-cases/user/register-user.use-case.ts
@@ -1,15 +1,24 @@
+import { JwtService } from '@nestjs/jwt';
 import { plainToInstance } from 'class-transformer';
 import { ConflictException, Injectable } from '@nestjs/common';
 
+import { envs } from '@shared/config';
 import { RegisterDto } from '@app/dto/auth';
 import { HashPassword } from '@shared/utils';
 import { UserRegisteredDTO } from '@app/dto/user/user.dto';
 import { UserRepository } from '@infrastructure/repositories/user.repository';
+import { EmailService } from '@infrastructure/email/services/email.service';
 
 @Injectable()
 export class RegisterUserUseCase {
-  constructor(private readonly userRepository: UserRepository) {}
-  async execute(dto: RegisterDto): Promise<UserRegisteredDTO> {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService,
+    private readonly emailService: EmailService,
+  ) {}
+  async execute(
+    dto: RegisterDto,
+  ): Promise<{ message: string; user: UserRegisteredDTO }> {
     const userExists = await this.userRepository.findUserByEmail(dto.email);
 
     if (userExists) {
@@ -21,8 +30,26 @@ export class RegisterUserUseCase {
     dto.password = HashPassword(dto.password);
     const user = await this.userRepository.create(dto);
 
-    return plainToInstance(UserRegisteredDTO, user, {
-      excludeExtraneousValues: true,
+    const logoUrl = `${envs.app.logoUrl}`;
+    const payload = { sub: user.id };
+    const token = this.jwtService.sign(payload, {
+      secret: envs.jwt.emailSecret,
+      expiresIn: '20m',
     });
+
+    const verificationUrl = `${envs.app.frontVerificationUrl}${token}`;
+
+    await this.emailService.sendVerificationEmail(
+      user,
+      verificationUrl,
+      logoUrl,
+    );
+
+    return {
+      message: 'Verification email sent',
+      user: plainToInstance(UserRegisteredDTO, user, {
+        excludeExtraneousValues: true,
+      }),
+    };
   }
 }
