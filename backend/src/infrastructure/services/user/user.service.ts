2c3324bce3e7f23b28cd571703545d06ed7b4a55
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-19 15:31:47 -0800

    feature: validation of email to new Users

--- backend/src/infrastructure/services/user/user.service.ts
+++ backend/src/infrastructure/services/user/user.service.ts
@@ -1,4 +1,11 @@
+import { VerifyUserUseCase } from '@app/use-cases/user/verify-email.use-case';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly verifyUserUseCase: VerifyUserUseCase) {}
+
+  async verifyToken(token: string) {
+    return this.verifyUserUseCase.execute(token);
+  }
+}
--- backend/src/presentation/controllers/user/user.controller.ts
+++ backend/src/presentation/controllers/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+
+import { UserService } from '@infrastructure/services/user/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('verify')
+  async verifyUser(@Query('token') token: string) {
+    if (!token) throw new BadRequestException('No token provided');
+    await this.userService.verifyToken(token);
+    return { message: 'Email verified' };
+  }
+}
