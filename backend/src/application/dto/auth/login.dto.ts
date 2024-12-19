4722c62b56245b4a6605fd3e966e5d84d82c43eb
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-19 08:51:05 -0800

    add: api documentation to login

--- backend/src/application/dto/auth/login.dto.ts
+++ backend/src/application/dto/auth/login.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(6)
   @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/, {
@@ -11,4 +14,10 @@ export class LoginDto {
       'Password must contain at least one uppercase letter, one lowercase letter, one number, and be at least 6 characters long',
   })
   password: string;
+}
+
+export class authResponseDto {
+  @ApiProperty()
+  @IsString()
+  access_token: string;
 }
--- backend/src/presentation/controllers/auth/auth.controller.ts
+++ backend/src/presentation/controllers/auth/auth.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 
-import { LoginDto } from '@app/dto/auth';
+import { authResponseDto, LoginDto } from '@app/dto/auth';
 import { AuthService } from '@infrastructure/services/auth/auth.service';
+import {
+  ApiResponse,
+  ApiNotFoundResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(200)
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiUnauthorizedResponse({ description: 'Invalid credentials' })
+  @ApiResponse({
+    status: 200,
+    description: 'Login successful',
+    type: authResponseDto,
+  })
   async login(@Body() data: LoginDto) {
     return this.authService.login(data);
   }
