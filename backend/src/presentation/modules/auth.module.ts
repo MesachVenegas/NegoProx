cca0d181fda442a43ec231fd9eebf07ee6db153b
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-18 17:12:39 -0800

    add: imports to auth module

--- backend/src/presentation/modules/auth.module.ts
+++ backend/src/presentation/modules/auth.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { AuthController } from '@presentation/controllers/auth/auth.controller';
+import { AuthService } from '@infrastructure/services/auth/auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { envs } from '@shared/config';
+import { UserModule } from './user.module';
+import { JwtBearerStrategy } from '@infrastructure/services/auth/strategy/jwt-bearer.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: envs.jwt.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+    UserModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtBearerStrategy],
+  exports: [AuthService],
+})
 export class AuthModule {}
