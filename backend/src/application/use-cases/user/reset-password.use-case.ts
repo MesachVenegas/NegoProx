ce437a20939477332961e2e88d7304227c86e89d
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-25 11:25:18 -0800

    create: use case to reset password of user

---
+++ backend/src/application/use-cases/user/reset-password.use-case.ts
@@ -0,0 +1,40 @@
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService, TokenExpiredError } from '@nestjs/jwt';
+
+import { envs } from '@shared/config';
+import { HashPassword } from '@shared/utils';
+import { UserRepository } from '@infrastructure/repositories/user.repository';
+import { ResetPasswordDto } from '@app/dto/user';
+
+@Injectable()
+export class ResetPasswordUseCase {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async execute(token: string, password: ResetPasswordDto['password']) {
+    let decoded: any;
+    try {
+      decoded = await this.jwtService.verify(token, {
+        secret: envs.jwt.emailSecret,
+      });
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        throw new ConflictException('Token expired');
+      }
+      throw new BadRequestException('Invalid token');
+    }
+
+    const existUser = await this.userRepository.findUserById(decoded.sub);
+    if (!existUser) {
+      throw new ConflictException('User not found or not exists');
+    }
+    const hashedPassword = HashPassword(password);
+    await this.userRepository.resetPassword(decoded.sub, hashedPassword);
+  }
+}
