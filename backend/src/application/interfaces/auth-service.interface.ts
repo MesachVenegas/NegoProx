213f02943822c100a182dab44be467f2a2d23bc1
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-25 11:23:22 -0800

    add: interface to services

---
+++ backend/src/application/interfaces/auth-service.interface.ts
@@ -0,0 +1,8 @@
+import { LoginDto, RegisterDto } from '@app/dto/auth';
+
+export interface AuthServiceInterface {
+  login(data: LoginDto): Promise<{ access_token: string }>;
+  register(
+    data: RegisterDto,
+  ): Promise<{ access_token: string; message: string }>;
+}
