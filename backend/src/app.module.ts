5f7c5d6e33e1f7ef60db1b45298c55788d72c3b0
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-10 17:00:49 -0800

    add: throttler protection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { PrismaModule } from '@infrastructure/database/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 60,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
