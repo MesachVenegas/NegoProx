7cadfcdbf8a0bd7afa4c870c262a35bde7004151
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-12 15:13:51 -0800

    changes: set configuration and throttler

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { envSchema } from './config/config.schema';
+import configuration from './config/configuration';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 10,
+      },
+    ]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.development.local', '.env.test.local'],
+      validationSchema: envSchema,
+      load: [configuration],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
