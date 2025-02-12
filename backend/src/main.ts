8a60d8921f8f9119732200fccbe7d8e63ffcdcd0
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-12 15:14:08 -0800

    add: main configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,34 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors('*');
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      validationError: { target: false, value: false },
+    }),
+  );
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: process.env.APP_VERSION,
+  });
+
+  await app.listen(process.env.PORT ?? 8000, () => {
+    Logger.debug(
+      `🚀 Application is running on: http://localhost:${process.env.APP_PORT ?? 8000}/api/v${process.env.APP_VERSION}`,
+    );
+  });
 }
 bootstrap();
