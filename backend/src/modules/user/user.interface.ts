438a3f9a487cd6707ab9b493bc0407374ac84d46
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-12 18:21:45 -0800

    add: interface to repository and query search

--- backend/src/modules/user/user.interface.ts
+++ backend/src/modules/user/user.interface.ts
@@ -1,6 +1,20 @@
+import { User } from './user.entity';
+
 export interface UpdateProfile {
   name?: string;
   lastName?: string;
   email?: string;
   phone?: string;
+}
+
+export interface FindQuery {
+  id?: string;
+  email?: string;
+  phone?: string;
+}
+
+export interface IUserRepository {
+  getAllUsers(): Promise<User[]>;
+  findUser(query: FindQuery): Promise<User | null>;
+  createLocalUser(data: User): Promise<User>;
 }
