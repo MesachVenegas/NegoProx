f2fd6f6d0817c98d8f68cc8a74789a43a8d34a38
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-13 16:15:18 -0800

    create: filter to handle http exceptions

---
+++ backend/src/shared/exceptions/http-exceptions.filter.ts
@@ -0,0 +1,30 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  Logger,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const message = exception.getResponse();
+
+    this.logger.error(
+      `HTTP Exception: ${JSON.stringify(message)} - ${request.url} - ${request.method} - ${request.ip}`,
+    );
+
+    response.status(status).json({
+      message,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
