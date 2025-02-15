4b1f4b58975a9e1d75ed2e2ab2a93f4b4d98d228
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-15 11:14:06 -0800

    feature: Prisma service optimization
    Prisma service check if database server is on hibernation state before to send a query request and reconnect if needed

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -12,6 +12,7 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   private readonly logger: Logger = new Logger(PrismaService.name);
+  private isConnected = false;
 
   constructor() {
     super({
@@ -23,26 +24,11 @@ export class PrismaService
       ],
     });
 
-    // development query logging
-    this.$on('query', (e) => {
-      this.logger.debug(`Query: ${e.query}`);
-      this.logger.debug(`Params: ${e.params}`);
-      this.logger.debug(`Duration: ${e.duration}ms`);
-    });
-
-    // development error logging
-    this.$on('error', (e) => {
-      this.logger.error(`Error: ${e.message}`);
-    });
+    this.setupLogging();
   }
+
   async onModuleInit() {
-    try {
-      await this.$connect();
-      this.logger.log('Database connection successful');
-    } catch (error) {
-      this.logger.error(`Failed to connect to database: ${error}`);
-      throw error;
-    }
+    await this.connectionWithRetry();
   }
 
   async onModuleDestroy() {
@@ -52,6 +38,67 @@ export class PrismaService
     } catch (error) {
       this.logger.error(`Failed to disconnect from database: ${error}`);
       throw error;
+    }
+  }
+
+  private setupLogging() {
+    this.$on('query', (e) => {
+      if (!this.isConnected) {
+        this.logger.log(`Database connection lost, Attempting to reconnect...`);
+        void this.reconnect();
+      }
+
+      this.logger.debug(`Query: ${e.query}`);
+      this.logger.debug(`Params: ${e.params}`);
+      this.logger.debug(`Duration: ${e.duration}ms`);
+    });
+
+    this.$on('error', (e) => {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        this.logger.error(
+          `Error: ${e.code} - ${e.name} - ${JSON.stringify(e.meta)}`,
+        );
+      } else {
+        if (e.message.includes('E57P01')) {
+          this.logger.warn(
+            `Warning: database server connection lost, database in on hibernation`,
+          );
+          // void this.reconnect();
+        }
+        this.logger.error(`Error: ${JSON.stringify(e.message)}`);
+      }
+    });
+  }
+
+  private async connectionWithRetry(retries = 5, delay = 5000) {
+    for (let attempt = 1; attempt <= retries; attempt++) {
+      try {
+        await this.$connect();
+        this.isConnected = true;
+        this.logger.log('Database connection established');
+        return;
+      } catch (error) {
+        this.logger.error(
+          `Database connection failed,(Attempt  ${attempt}/${retries} failed: ${error})`,
+        );
+        this.isConnected = false;
+        if (attempt < retries) {
+          this.logger.warn(`Retrying in ${delay / 1000} seconds...`);
+          await new Promise((resolve) => setTimeout(resolve, delay));
+        } else {
+          throw new Error('Database connection failed, after all retries.');
+        }
+      }
+    }
+  }
+
+  private async reconnect() {
+    this.isConnected = false;
+    try {
+      await this.$disconnect();
+      await this.connectionWithRetry();
+    } catch (error) {
+      this.logger.error(`Failed to reconnect to database: ${error}`);
     }
   }
 }
