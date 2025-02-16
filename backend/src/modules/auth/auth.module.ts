379cb4dcf9c20edbfc993e0262c493e6159dc21b
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-15 19:20:51 -0800

    change: vars load

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,25 +1,25 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserRepository } from '../user/user.repository';
 import { TokenVersionModule } from '../token-version/token-version.module';
+import { AuthRepository } from './auth.repository';
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      useFactory: (conf: ConfigService) => ({
-        secret: conf.get('jwt.JWT_SECRET'),
-        signOptions: { expiresIn: conf.get('jwt.JWT_EXPIRES_IN') },
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET || 'secret',
+        signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '30m' },
       }),
     }),
     TokenVersionModule,
   ],
-  providers: [AuthService, UserRepository],
+  providers: [AuthService, UserRepository, AuthRepository],
   controllers: [AuthController],
   exports: [AuthService],
 })
