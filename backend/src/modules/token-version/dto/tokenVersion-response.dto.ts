8aee80544c5ac00b6eba7314506eb1fa2f3e1ab3
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-17 12:25:53 -0800

    add: swagger doc and dto validation

--- backend/src/modules/token-version/dto/tokenVersion-response.dto.ts
+++ backend/src/modules/token-version/dto/tokenVersion-response.dto.ts
@@ -1,4 +1,6 @@
+import { ResponseUserDto } from '@/modules/user/dto/user-response.dto';
 import { ApiProperty } from '@nestjs/swagger';
+import { Exclude, Type } from 'class-transformer';
 import { IsDate, IsNumber, IsString } from 'class-validator';
 
 export class TokenVersionDto {
@@ -11,4 +13,17 @@ export class TokenVersionDto {
   @ApiProperty({ example: '2023-01-01T00:00:00.000Z' })
   @IsDate()
   updatedAt: Date;
+  @Exclude()
+  @IsString()
+  userId: string;
+
+  @Exclude()
+  @Type(() => ResponseUserDto)
+  user: ResponseUserDto;
+
+  constructor(partial: Partial<TokenVersionDto>) {
+    this.id = partial.id ?? '';
+    this.version = partial.version ?? 0;
+    this.updatedAt = partial.updatedAt ?? new Date();
+  }
 }
