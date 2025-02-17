3758097c25e1bb4b520978b7b89d29a0daff27dc
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-17 12:29:08 -0800

    add: find user to validate local login

--- backend/src/modules/auth/auth.repository.ts
+++ backend/src/modules/auth/auth.repository.ts
@@ -9,46 +9,73 @@ export class AuthRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   /**
-   * Authenticates a user via their Google account.
+   * Retrieves a user's profile and token version by their email address.
    *
-   * This method searches for an existing user account associated with a given Google profile.
-   * If an account is found, the associated user is returned. If not, a new user is created
-   * using the profile information provided.
-   *
-   * @param data - The Google profile data containing the user's ID, email, and display name.
-   * @returns A promise that resolves with the authenticated or newly created user.
+   * @param email - The email address of the user to retrieve.
+   * @returns The user's profile, including their token version, or null if the user does not exist.
    */
-  async authenticateGoogleAccount(data: Profile): Promise<UserProfileAccDto> {
-    const email = data.emails[0];
+  async findLocalUser(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+      include: { userProfile: true, accounts: true },
+    });
+    return user;
+  }
+
+  /**
+   * Authenticates a user using their Google account profile.
+   *
+   * This method checks if a Google account with the given profile ID exists
+   * in the database. If an account exists, it returns the associated user.
+   * If no account is found, it creates a new user with the provided email,
+   * display name, and associates it with a new Google account and user profile.
+   * The newly created or existing user is returned with their profile and accounts.
+   *
+   * @param profile - The profile information from Google.
+   * @returns The authenticated user object, either existing or newly created.
+   */
+
+  async authenticateGoogleAccount(
+    profile: Profile,
+  ): Promise<UserProfileAccDto> {
+    const { id, emails, displayName } = profile;
+    const email: string = emails[0].value;
     const existing = await this.prisma.account.findUnique({
       where: {
         provider_providerId: {
           provider: 'google',
-          providerId: data.id,
+          providerId: id,
         },
       },
-      include: { user: { include: { userProfile: true, accounts: true } } },
+      include: {
+        user: {
+          include: { tokenVersion: true, userProfile: true, accounts: true },
+        },
+      },
     });
 
     if (existing) return new UserProfileAccDto(existing.user);
 
+    const [name, ...lastNameParts] = displayName.split(' ');
+    const lastName = lastNameParts.join(' ') || '';
     const user = await this.prisma.user.upsert({
       where: { email },
       update: {},
       create: {
         email,
-        name: data.displayName,
-        lastName: '',
+        name,
+        lastName,
         emailVerified: true,
         accounts: {
           create: {
             provider: 'google',
-            providerId: data.id,
+            providerId: id,
           },
         },
         userProfile: { create: {} },
+        tokenVersion: { create: {} },
       },
-      include: { userProfile: true, accounts: true },
+      include: { tokenVersion: true, userProfile: true },
     });
 
     return new UserProfileAccDto(user);
