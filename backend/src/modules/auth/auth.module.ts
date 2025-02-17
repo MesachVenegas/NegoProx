966f6443ce05144cf58f03e7725f1c9f6213e884
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-17 15:52:25 -0800

    changes:
    - implement configService to load environment variables
    - add auth strategies Local,Google and Jwt

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
@@ -7,19 +8,32 @@ import { AuthController } from './auth.controller';
 import { UserRepository } from '../user/user.repository';
 import { TokenVersionModule } from '../token-version/token-version.module';
 import { AuthRepository } from './auth.repository';
+import { LocalStrategy } from './strategy/local.strategy';
+import { GoogleStrategy } from './strategy/google.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      useFactory: () => ({
-        secret: process.env.JWT_SECRET || 'secret',
-        signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '30m' },
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('security.jwrSecret') || 'secret',
+        signOptions: {
+          expiresIn: config.get<string>('security.jwtExpiresIn') || '1h',
+        },
       }),
+      inject: [ConfigService],
     }),
     TokenVersionModule,
   ],
-  providers: [AuthService, UserRepository, AuthRepository],
+  providers: [
+    AuthService,
+    UserRepository,
+    AuthRepository,
+    LocalStrategy,
+    GoogleStrategy,
+    JwtStrategy,
+  ],
   controllers: [AuthController],
   exports: [AuthService],
 })
