823c4412e858f60d5c9b0cbf7ea284da67b0483e
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-17 19:09:26 -0800

    add: function to ignore routes marked as public

--- backend/src/modules/auth/guards/jwt.guard.ts
+++ backend/src/modules/auth/guards/jwt.guard.ts
@@ -1,13 +1,32 @@
+import { IS_PUBLIC_KEY } from '@/shared/core/decorators/public.decorator';
 import {
   Injectable,
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { TokenExpiredError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(
+    ctx: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      ctx.getHandler(),
+      ctx.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    return super.canActivate(ctx);
+  }
   handleRequest(
     err: unknown,
     user: any,
