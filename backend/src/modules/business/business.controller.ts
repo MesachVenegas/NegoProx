bdb92e69bd7caac32480de9d032e6487384b84fc
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-19 10:48:50 -0800

    add: swagger documentations

--- backend/src/modules/business/business.controller.ts
+++ backend/src/modules/business/business.controller.ts
@@ -7,15 +7,53 @@ import {
   PaginationResponseDto,
 } from '@/shared/dto/pagination.dto';
 import { BusinessResponseDto } from './dto/business-response.dto';
+import {
+  ApiBadRequestResponse,
+  ApiExtraModels,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import { HttpErrorResponseDto } from '@/shared/dto/http-error-response.dto';
 // import { BusinessProfileDto } from './dto/business-profile.dto';
 
 @Controller('business')
 @UseGuards(JwtGuard)
+@ApiNotFoundResponse({
+  description: 'Business not found',
+  type: HttpErrorResponseDto,
+})
+@ApiUnauthorizedResponse({
+  description: 'Unauthorized required session',
+  type: HttpErrorResponseDto,
+})
+@ApiBadRequestResponse({
+  description: 'Bad request error',
+  type: HttpErrorResponseDto,
+})
+@ApiExtraModels(PaginationResponseDto, BusinessResponseDto)
 export class BusinessController {
   constructor(private readonly businessService: BusinessService) {}
 
   @Get()
   @Public()
+  @ApiOkResponse({
+    description: 'List of businesses available',
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(PaginationResponseDto) },
+        {
+          properties: {
+            data: {
+              type: 'array',
+              items: { $ref: getSchemaPath(BusinessResponseDto) },
+            },
+          },
+        },
+      ],
+    },
+  })
   async loadBusiness(
     @Query() query: PaginationDto,
   ): Promise<PaginationResponseDto<BusinessResponseDto[]>> {
@@ -24,6 +62,10 @@ export class BusinessController {
 
   @Get(':id')
   @Public()
+  @ApiOkResponse({
+    description: 'Business found',
+    type: BusinessResponseDto,
+  })
   async findBusiness(@Param('id') id: string) {
     return await this.businessService.findBusinessById(id);
   }
