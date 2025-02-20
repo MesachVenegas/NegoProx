2f62d1ad35db455ba920db19e6481fc88eed902c
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-19 18:16:57 -0800

    add: providers to module

--- backend/src/modules/business/business.module.ts
+++ backend/src/modules/business/business.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { BusinessService } from './business.service';
 import { BusinessController } from './business.controller';
 import { BusinessRepository } from './business.repository';
+import { UserService } from '../user/user.service';
+import { UserRepository } from '../user/user.repository';
 
 @Module({
-  providers: [BusinessService, BusinessRepository],
+  providers: [BusinessService, BusinessRepository, UserService, UserRepository],
   controllers: [BusinessController],
   exports: [BusinessService],
 })
