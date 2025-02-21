7582b54959bd45b88dbf5054805e649c56e15f40
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-20 16:24:00 -0800

    changes: use filter by name and categories, and implements pagination dto

--- backend/src/modules/business/dto/search-business.dto.ts
+++ backend/src/modules/business/dto/search-business.dto.ts
@@ -1,30 +1,15 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Decimal } from '@prisma/client/runtime/library';
-import { Transform, Type } from 'class-transformer';
+import { PaginationDto } from '@/shared/dto/pagination.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
-export class SearchBusinessDto {
-  @ApiProperty({ required: false, example: '1' })
-  @IsOptional()
-  @IsString()
-  id?: string;
-
-  @ApiProperty({ required: false, example: 'Pizza Hut' })
+export class SearchBusinessDto extends PaginationDto {
+  @ApiPropertyOptional({ example: 'Pizza Hut' })
   @IsOptional()
   @IsString()
   name?: string;
-  @ApiProperty({ required: false, example: '112.232132' })
+
+  @ApiPropertyOptional({ example: 'Pizzeria' })
   @IsOptional()
-  @Type(() => Number)
-  @Transform(({ value }: { value: unknown }) =>
-    value instanceof Decimal ? value.toNumber() : value,
-  )
-  latitude?: number;
-  @ApiProperty({ required: false, example: '-77.232132' })
-  @IsOptional()
-  @Type(() => Number)
-  @Transform(({ value }: { value: unknown }) =>
-    value instanceof Decimal ? value.toNumber() : value,
-  )
-  longitude?: number;
+  @IsString()
+  category?: string;
 }
