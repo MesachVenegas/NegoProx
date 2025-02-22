6820d683a51e6c302152cb2eab53cc1cec23c612
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-21 19:03:47 -0800

    add: relations property to entity

--- backend/src/modules/user/user.entity.ts
+++ backend/src/modules/user/user.entity.ts
@@ -1,7 +1,14 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, Type } from 'class-transformer';
 import { Account } from '../account/account.entity';
-import { UserProfile } from '../user-profile/user-profile.entity';
 import { Role } from '@/shared/constants/role.enum';
+import { UserProfile } from '../user-profile/user-profile.entity';
+import { Business } from '../business/business.entity';
+import { Appointment } from '../appointment/appointment.entity';
+import { Work } from '../work/work.entity';
+import { Payment } from '../payment/payment.entity';
+import { Review } from '../review/review.entity';
+import { TokenVersion } from '../token-version/token-version.entity';
+import { Verification } from '../verification/verification.entity';
 
 export class User {
   id: string;
@@ -15,6 +22,44 @@ export class User {
   isDisabled: boolean;
   userType: Role;
   registeredAt: Date;
-  userProfile?: UserProfile | null;
-  accounts?: Account[] | null;
+
+  @Type(() => UserProfile)
+  userProfile?: UserProfile;
+
+  @Type(() => Account)
+  accounts?: Account[];
+
+  @Type(() => Business)
+  businesses?: Business[];
+
+  sentMessages?: any[];
+  receivedMessages?: any[];
+
+  @Type(() => Appointment)
+  clientAppointments?: Appointment[];
+
+  @Type(() => Work)
+  clientWorks?: Work[];
+
+  @Type(() => Payment)
+  payments?: Payment[];
+
+  @Type(() => Review)
+  reviews?: Review[];
+
+  @Type(() => TokenVersion)
+  tokenVersion?: TokenVersion;
+
+  @Type(() => Verification)
+  verification?: Verification[];
+
+  conversationAsClient?: any[];
+  conversationAsBusiness?: any[];
+
+  update(partial: Partial<User>) {
+    delete partial.id;
+    delete partial.registeredAt;
+
+    Object.assign(this, partial);
+  }
 }
