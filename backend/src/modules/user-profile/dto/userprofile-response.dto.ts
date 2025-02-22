46c8bf8213be05674728be4dc8d104c60f76985a
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-21 19:04:06 -0800

    typo: spaces

--- backend/src/modules/user-profile/dto/userprofile-response.dto.ts
+++ backend/src/modules/user-profile/dto/userprofile-response.dto.ts
@@ -5,18 +5,23 @@ export class UserProfileResponseDto {
   @ApiProperty({ example: '1' })
   @IsString()
   id: string;
+
   @ApiProperty({ example: 'https://example.com/profile.jpg' })
   @IsString()
   profilePicture: string | null;
+
   @ApiProperty({ example: 'My bio' })
   @IsString()
   bio: string | null;
+
   @ApiProperty({ example: '123 Main St, Anytown, USA' })
   @IsString()
   address: string | null;
+
   @ApiProperty({ example: '2023-01-01T00:00:00.000Z' })
   @IsDate()
   createdAt: Date;
+
   @ApiProperty({ example: '2023-01-01T00:00:00.000Z' })
   @IsDate()
   updatedAt: Date;
