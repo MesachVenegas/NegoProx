bf2470e1ebcb9e1ec4d6b521e8ba67e317ee6796
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-24 08:50:39 -0800

    changes: rename file of repository

---
+++ backend/src/domain/interfaces/user-repository.ts
@@ -0,0 +1,22 @@
+import { User } from '../entities';
+
+export interface UserRepository {
+  countUsers(): Promise<number>;
+  getAllUsers(
+    skip: number,
+    limit: number,
+    order: 'asc' | 'desc',
+  ): Promise<User[] | null>;
+  findUserById(id: string): Promise<User | null>;
+  findUserByEmail(email: string): Promise<User | null>;
+  searchUserByQuery(
+    id?: string,
+    email?: string,
+    phone?: string,
+  ): Promise<User | null>;
+  saveLocalUser(data: User): Promise<User>;
+  updateUser(user: User): Promise<User>;
+  updatePassword(password: string, id: string): Promise<User>;
+  disableAccount(id: string): Promise<User>;
+  logicDeleteAccount(id: string): Promise<User>;
+}
