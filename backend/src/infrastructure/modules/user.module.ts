0623b3853c3c86d629f22a30d0771509d47d00bb
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-24 08:53:07 -0800

    change: update repository name, and remoce user service

--- backend/src/infrastructure/modules/user.module.ts
+++ backend/src/infrastructure/modules/user.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { UserService } from '@application/user/use-cases/user.service';
 import { UserController } from '@infrastructure/controllers/user.controller';
-import { UserRepository } from '@infrastructure/repositories/user.repository';
+import { UserPrismaRepository } from '../repositories/user.repository';
 
 @Module({
-  providers: [UserService, UserRepository],
+  providers: [UserPrismaRepository],
   controllers: [UserController],
-  exports: [UserService, UserRepository],
+  exports: [UserPrismaRepository],
 })
 export class UserModule {}
