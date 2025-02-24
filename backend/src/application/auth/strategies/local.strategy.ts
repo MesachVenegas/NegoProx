e56767d5e3732b89660d3b4263b00ceb71d234ce
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-23 19:59:44 -0800

    changes: update import file paths

--- backend/src/application/auth/strategies/local.strategy.ts
+++ backend/src/application/auth/strategies/local.strategy.ts
@@ -1,10 +1,10 @@
 import { Strategy } from 'passport-local';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-
-import { AuthService } from '../auth.service';
 import { plainToInstance } from 'class-transformer';
-import { UserTokenVersionDto } from '@/modules/user/dto/user-token.dto';
+
+import { AuthService } from '../use-case/auth.service';
+import { UserTokenVersionDto } from '@/infrastructure/dto/user/user-token.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
