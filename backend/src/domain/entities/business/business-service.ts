c0e1571d2dc995a4a280f83e9446489a825cf2bd
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-25 15:38:01 -0800

    add: constructor to entity

--- backend/src/domain/entities/business/business-service.ts
+++ backend/src/domain/entities/business/business-service.ts
@@ -1,14 +1,34 @@
 import { Business } from './business';
 
 export class BusinessService {
-  public id: string;
+  public id: string | undefined;
   public name: string;
   public price: number;
-  public description: string;
+  public description?: string;
   public time: number;
   public isDisabled: boolean;
-  public businessId: string;
+  public businessId?: string;
   public business?: Business;
+
+  constructor(params: {
+    id?: string;
+    name: string;
+    price: number;
+    description?: string | null;
+    time: number;
+    isDisabled?: boolean;
+    businessId?: string;
+    business?: Business;
+  }) {
+    this.id = params.id;
+    this.name = params.name;
+    this.price = params.price;
+    this.description = params.description ?? '';
+    this.time = params.time;
+    this.isDisabled = params.isDisabled ?? false;
+    this.businessId = params.businessId;
+    this.business = params.business;
+  }
 
   /**
    * Updates the service with the provided data.
