0902cbd6591cd5355f9957b87c934b9d000320bd
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-25 14:02:26 -0800

    add: explicit constructor

--- backend/src/domain/entities/business/business.ts
+++ backend/src/domain/entities/business/business.ts
@@ -11,7 +11,7 @@ import { BusinessService } from './business-service';
 import { BusinessCategory } from './business-category';
 
 export class Business {
-  public id: string;
+  public id: string | undefined;
   public name: string;
   public description?: string;
   public address?: string;
@@ -33,8 +33,60 @@ export class Business {
   public categories?: BusinessCategory[];
   public availability?: Availability[];
   public images?: BusinessImage[];
-  public payments: Payment[];
+  public payments?: Payment[];
   public reviews?: Review[];
+
+  constructor(init: {
+    id?: string;
+    name: string;
+    description?: string | null;
+    address?: string | null;
+    latitude?: number | null;
+    longitude?: number | null;
+    phone?: string | null;
+    imgProfile?: string | null;
+    isDisabled?: boolean | null;
+    isDeleted?: boolean | null;
+    createdAt?: Date | null;
+    updatedAt?: Date | null;
+
+    user?: User;
+    userId?: string;
+    services?: BusinessService[];
+    appointments?: Appointment[];
+    works?: Work[];
+    businessProfile?: BusinessProfile;
+    categories?: BusinessCategory[];
+    availability?: Availability[];
+    images?: BusinessImage[];
+    payments?: Payment[];
+    reviews?: Review[];
+  }) {
+    this.id = init.id;
+    this.name = init.name;
+    this.description = init.description ?? undefined;
+    this.address = init.address ?? undefined;
+    this.latitude = init.latitude ?? 0;
+    this.longitude = init.longitude ?? 0;
+    this.phone = init.phone ?? undefined;
+    this.imgProfile = init.imgProfile ?? undefined;
+    this.isDisabled = init.isDisabled ?? false;
+    this.isDeleted = init.isDeleted ?? false;
+    this.createdAt = init.createdAt ?? new Date();
+    this.updatedAt = init.updatedAt ?? new Date();
+
+    this.user = init.user;
+    this.userId = init.userId;
+    this.services = init.services;
+    this.appointments = init.appointments;
+    this.works = init.works;
+    this.businessProfile = init.businessProfile;
+    this.categories = init.categories;
+    this.availability = init.availability;
+    this.images = init.images;
+    this.payments = init.payments;
+    this.reviews = init.reviews;
+  }
 
   update(partial: Partial<Business>) {
     delete partial.id;
