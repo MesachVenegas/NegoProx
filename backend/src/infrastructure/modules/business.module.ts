963b28ac94617c2585ebaa9741341879a07089a8
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-25 14:02:53 -0800

    change: update repository name

--- backend/src/infrastructure/modules/business.module.ts
+++ backend/src/infrastructure/modules/business.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { BusinessService } from '@application/business/business.service';
 import { UserPrismaRepository } from '@infrastructure/repositories/user.repository';
 import { BusinessController } from '@infrastructure/controllers/business.controller';
-import { BusinessRepository } from '@infrastructure/repositories/business.repository';
+import { BusinessPrismaRepository } from '@infrastructure/repositories/business.repository';
 
 @Module({
-  providers: [BusinessService, BusinessRepository, UserPrismaRepository],
+  providers: [BusinessPrismaRepository, UserPrismaRepository],
   controllers: [BusinessController],
-  exports: [BusinessService, BusinessRepository],
+  exports: [BusinessPrismaRepository],
 })
 export class BusinessModule {}
