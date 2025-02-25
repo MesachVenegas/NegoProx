eb3a328a0e9ceba433d0f605a922363fb08754df
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-25 05:38:24 -0800

    change: remove logic and only processing request to DB

--- backend/src/infrastructure/repositories/auth.repository.ts
+++ backend/src/infrastructure/repositories/auth.repository.ts
@@ -1,71 +1,52 @@
 import { Injectable } from '@nestjs/common';
 import { Profile } from 'passport-google-oauth20';
-import { plainToInstance } from 'class-transformer';
 
-import { User } from '@/domain/entities';
+import { Role } from '@/domain/constants/role.enum';
 import { PrismaService } from '@/infrastructure/orm/prisma.service';
+import { AuthRepository } from '@/domain/interfaces/auth-repository';
+import { Account, TokenVersion, User, UserProfile } from '@/domain/entities';
 
 @Injectable()
-export class AuthRepository {
+export class AuthPrismaRepository implements AuthRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   /**
-   * Retrieves a user's profile and token version by their email address.
+   * Retrieves a user by their email address if the user is a local user.
    *
    * @param email - The email address of the user to retrieve.
-   * @returns The user's profile, including their token version, or null if the user does not exist.
+   * @returns A promise that resolves with the User object if found.
    */
   async findLocalUser(email: string) {
     const user = await this.prisma.user.findUnique({
       where: { email },
       include: { userProfile: true, accounts: true, tokenVersion: true },
     });
-    return user;
+
+    if (!user) return null;
+
+    return new User({
+      ...user,
+      userType: user.userType as Role,
+      tokenVersion: user.tokenVersion as TokenVersion,
+      userProfile: user.userProfile as UserProfile,
+      accounts: user.accounts as Account[],
+    });
   }
 
-  /**
-   * Authenticates a user using their Google account profile.
-   *
-   * This method checks if a Google account with the given profile ID exists
-   * in the database. If an account exists, it returns the associated user.
-   * If no account is found, it creates a new user with the provided email,
-   * display name, and associates it with a new Google account and user profile.
-   * The newly created or existing user is returned with their profile and accounts.
-   *
-   * @param profile - The profile information from Google.
-   * @returns The authenticated user object, either existing or newly created.
-   */
-
-  async authenticateGoogleAccount(profile: Profile) {
-    const { id, emails, name, photos } = profile;
-    const email = emails?.[0].value;
+  async registerGoogleAccount(profile: Profile) {
+    const { id, emails, name, photos, provider } = profile;
+    const email = emails?.[0].value as string;
     const verified = emails?.[0].verified;
-    const existing = await this.prisma.account.findUnique({
-      where: {
-        provider_providerId: {
-          provider: 'google',
-          providerId: id,
-        },
-      },
-      include: {
-        user: {
-          include: { tokenVersion: true, userProfile: true, accounts: true },
-        },
-      },
-    });
 
-    if (existing) return plainToInstance(User, existing.user);
-    const user = await this.prisma.user.upsert({
-      where: { email },
-      update: {},
-      create: {
-        email: email as string,
+    const user = await this.prisma.user.create({
+      data: {
+        email: email,
         name: name?.givenName,
         lastName: name?.familyName,
         emailVerified: verified,
         accounts: {
           create: {
-            provider: 'google',
+            provider,
             providerId: id,
           },
         },
@@ -79,6 +60,11 @@ export class AuthRepository {
       include: { tokenVersion: true, userProfile: true },
     });
 
-    return plainToInstance(User, user);
+    return new User({
+      ...user,
+      userType: user.userType as Role,
+      userProfile: user.userProfile as UserProfile,
+      tokenVersion: user.tokenVersion as TokenVersion,
+    });
   }
 }
