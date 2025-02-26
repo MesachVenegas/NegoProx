9a44510e9d66cb7b6d4c5d65447bd4243e3279ee
MesachVenegas <mesach.venegas@hotmail.com>
2025-02-25 16:54:50 -0800

    create: business service interface

--- backend/src/infrastructure/repositories/business-service.repository.ts
+++ backend/src/infrastructure/repositories/business-service.repository.ts
@@ -1,32 +1,44 @@
 import { Injectable } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
 
-import { BusinessService } from '@/domain/entities/business';
+import { Business, BusinessService } from '@/domain/entities/business';
 import { PrismaService } from '@/infrastructure/orm/prisma.service';
+import { BusinessServicesRepository } from '@/domain/interfaces/services-repository';
 
 @Injectable()
-export class BusinessServicesRepository {
+export class BusinessServicesPrismaRepository
+  implements BusinessServicesRepository
+{
   constructor(private readonly prisma: PrismaService) {}
 
   /**
-   * Retrieves a list of all services in the database, excluding disabled services.
+   * Retrieves all services from the database, sorted by name in ascending order.
    *
-   * @returns A promise that resolves with an array of Service objects.
+   * @returns A promise that resolves with an array of BusinessService objects, or null if no services are found.
    */
-  async getAllService() {
+  async getAllServices() {
     const result = await this.prisma.service.findMany({
       where: { isDisabled: false },
       orderBy: { name: 'asc' },
     });
 
-    return plainToInstance(BusinessService, result);
+    if (result.length === 0) return null;
+
+    const service = result.map(
+      (service) =>
+        new BusinessService({
+          ...service,
+          price: service.price.toNumber(),
+        }),
+    );
+
+    return service;
   }
 
   /**
-   * Retrieves a list of services associated with a given business ID.
+   * Retrieves all services of the given business from the database.
    *
-   * @param id - The unique identifier of the business to retrieve services for.
-   * @returns A promise that resolves with an array of Service objects associated with the business.
+   * @param id - The unique identifier of the business to retrieve its services.
+   * @returns A promise that resolves with an array of BusinessService objects, or null if no services are found.
    */
   async getServicesByBusinessId(id: string) {
     const result = await this.prisma.service.findMany({
@@ -34,14 +46,29 @@ export class BusinessServicesRepository {
       include: { business: true },
     });
 
-    return plainToInstance(BusinessService, result);
+    if (!result || result.length === 0) return null;
+
+    const service = result.map(
+      (service) =>
+        new BusinessService({
+          ...service,
+          price: service.price.toNumber(),
+          business: new Business({
+            ...service.business,
+            latitude: service.business.latitude?.toNumber(),
+            longitude: service.business.longitude?.toNumber(),
+          }),
+        }),
+    );
+
+    return service;
   }
 
   /**
-   * Retrieves a service by its ID.
+   * Retrieves a service by its ID from the database.
    *
    * @param id - The unique identifier of the service to retrieve.
-   * @returns A promise that resolves with a Service object, or throws a NotFoundException if the service is not found.
+   * @returns A promise that resolves with a BusinessService object, or null if no service is found.
    */
   async getServiceById(id: string) {
     const result = await this.prisma.service.findUnique({
@@ -49,14 +76,26 @@ export class BusinessServicesRepository {
       include: { business: true },
     });
 
-    return plainToInstance(BusinessService, result);
+    if (!result) return null;
+
+    const service = new BusinessService({
+      ...result,
+      price: result.price.toNumber(),
+      business: new Business({
+        ...result.business,
+        latitude: result.business.latitude?.toNumber(),
+        longitude: result.business.longitude?.toNumber(),
+      }),
+    });
+
+    return service;
   }
 
   /**
    * Creates a new service and associates it with a business.
    *
-   * @param service - The service entity containing the data for the new service.
-   * @returns A promise that resolves with the newly created Service object.
+   * @param service - The BusinessService object to create.
+   * @returns A promise that resolves with the created BusinessService object, or null if no service is created.
    */
   async createService(service: BusinessService) {
     const result = await this.prisma.service.create({
@@ -70,14 +109,26 @@ export class BusinessServicesRepository {
       include: { business: true },
     });
 
-    return plainToInstance(BusinessService, result);
+    if (!result) return null;
+
+    const newService = new BusinessService({
+      ...result,
+      price: result.price.toNumber(),
+      business: new Business({
+        ...result.business,
+        latitude: result.business.latitude?.toNumber(),
+        longitude: result.business.longitude?.toNumber(),
+      }),
+    });
+
+    return newService;
   }
 
   /**
-   * Updates an existing service entity with new data.
+   * Updates a service in the database with the provided data.
    *
-   * @param service - The service entity containing updated data.
-   * @returns A promise that resolves with the updated Service object.
+   * @param service - The BusinessService object with the updated data.
+   * @returns A promise that resolves with the updated BusinessService object, or null if no service is updated.
    */
   async updateService(service: BusinessService) {
     const result = await this.prisma.service.update({
@@ -90,45 +141,53 @@ export class BusinessServicesRepository {
       },
     });
 
-    return plainToInstance(BusinessService, result);
+    const updatedService = new BusinessService({
+      ...result,
+      price: result.price.toNumber(),
+    });
+
+    return updatedService;
   }
 
   /**
-   * Disables a service, effectively removing it from the service list.
+   * Disables a service in the database, effectively marking it as inactive.
    *
    * @param id - The unique identifier of the service to disable.
-   * @returns A promise that resolves with the updated Service object.
+   * @returns A promise that resolves with true if the service is disabled, otherwise false.
    */
-  async disableService(id: string) {
+  async disableService(id: string): Promise<boolean> {
     const result = await this.prisma.service.update({
       where: { id },
       data: { isDisabled: true },
     });
 
-    return plainToInstance(BusinessService, result);
+    if (!result) return false;
+
+    return true;
   }
 
   /**
-   * Deletes a service by its ID.
+   * Deletes a service from the database.
    *
-   * This method permanently removes the service from the database.
    * @param id - The unique identifier of the service to delete.
-   * @returns A promise that resolves with the deleted Service object.
+   * @returns A promise that resolves with true if the service is deleted, otherwise false.
    */
   async deleteService(id: string) {
     const result = await this.prisma.service.delete({
       where: { id },
     });
 
-    return plainToInstance(BusinessService, result);
+    if (!result) return false;
+
+    return true;
   }
 
   /**
-   * Filters a service by its name and business ID.
+   * Retrieves a service by its name and business ID from the database.
    *
-   * @param name - The name of the service to filter.
-   * @param businessId - The unique identifier of the business whose services are to be filtered.
-   * @returns A promise that resolves with the filtered Service object, or null if no service is found.
+   * @param name - The name of the service to retrieve.
+   * @param businessId - The unique identifier of the business associated with the service.
+   * @returns A promise that resolves with a BusinessService object, or null if no service is found.
    */
   async filterServiceByNameAndBusiness(name: string, businessId: string) {
     const result = await this.prisma.service.findUnique({
@@ -137,6 +196,13 @@ export class BusinessServicesRepository {
       },
     });
 
-    return plainToInstance(BusinessService, result);
+    if (!result) return null;
+
+    const service = new BusinessService({
+      ...result,
+      price: result.price.toNumber(),
+    });
+
+    return service;
   }
 }
