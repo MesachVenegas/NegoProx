1b90245e0bc43a84fad83bf61208fb228102dd88
MesachVenegas <mesach.venegas@hotmail.com>
2025-03-01 10:08:11 -0800

    chages: to retrieve a array type

--- backend/src/domain/interfaces/availability-repository.ts
+++ backend/src/domain/interfaces/availability-repository.ts
@@ -1,9 +1,12 @@
 import { Availability } from '../entities/business/business-availability';
 
 export interface AvailabilityRepository {
-  getAvailabilityByBusinessId(businessId: string): Promise<Availability | null>;
+  getAvailabilityByBusinessId(
+    businessId: string,
+  ): Promise<Availability[] | null>;
   getAvailabilityByServiceId(serviceId: string): Promise<Availability | null>;
-  saveAvailability(availability: Availability): Promise<Availability>;
+  getAvailabilityById(id: string): Promise<Availability | null>;
+  saveAvailability(availability: Availability[]): Promise<Availability[]>;
   updateAvailability(availability: Availability): Promise<Availability>;
   deleteAvailability(id: string): Promise<Availability>;
   addServiceToAvailability(
