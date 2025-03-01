84b1411374f1349ea464debf9d1ad868048d0b3e
MesachVenegas <mesach.venegas@hotmail.com>
2025-03-01 09:49:20 -0800

    fix: change property to represent availabilities and fix the bad assignation on data

--- backend/src/infrastructure/dto/business/business-profile.dto.ts
+++ backend/src/infrastructure/dto/business/business-profile.dto.ts
@@ -1,13 +1,13 @@
 import { Type } from 'class-transformer';
+import { IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-import { BusinessResponseDto } from './business-response.dto';
+import { AvailabilityResponseDto } from '../availability';
 import { BusinessProfileDto } from './profile-response.dto';
+import { BusinessResponseDto } from './business-response.dto';
 import { BusinessImageResponseDto } from './images-response.dto';
 import { BusinessServicesResponseDto } from './services-response.dto';
 import { BusinessCategoryResponseDto } from './categories-response.dto';
-import { IsNumber } from 'class-validator';
-import { Availability } from '@/domain/entities/business';
 
 export class BusinessProfileResponseDto extends BusinessResponseDto {
   @ApiProperty({ type: [BusinessImageResponseDto] })
@@ -26,9 +26,9 @@ export class BusinessProfileResponseDto extends BusinessResponseDto {
   @Type(() => BusinessCategoryResponseDto)
   categories?: BusinessCategoryResponseDto[];
 
-  @ApiProperty({ type: [Availability] })
-  @Type(() => Availability)
-  availabilities?: Availability[];
+  @ApiProperty({ type: [AvailabilityResponseDto] })
+  @Type(() => AvailabilityResponseDto)
+  availability?: AvailabilityResponseDto[];
 
   @ApiProperty({ example: '1' })
   @IsNumber()
