e915143b5116df1360d3d5ecf56e7d144e484ed9
MesachVenegas <mesach.venegas@hotmail.com>
2025-03-03 09:20:44 -0800

    clear

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+	weight: ["400", "500", "600", "700"],
+	subsets: ["latin", "latin-ext"],
+	variable: "--font-poppins",
+	display: "swap",
+	preload: true,
+});
+
+export const metadata: Metadata = {
+	title: "NegoProx - Conectando negocios",
+	description:
+		"Conectando negocios, desde la comodidad de tu hogar y facilitando la comunicación con tus clientes.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="es">
+			<body className={`${poppins.variable} antialiased`}>
+				<div>{children}</div>
+			</body>
+		</html>
+	);
+}
