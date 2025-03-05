5b0e54c31f30af760fd29fb689904a093157858f
MesachVenegas <mesach.venegas@hotmail.com>
2025-03-05 07:40:59 -0800

    typo: commnet

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
 export default createMiddleware(routing);
-
+// TODO: Fix language change, doesn't work fine, need reload page to see changes
 export const config = {
 	matcher: ["/", "/(es|en)/:path*"],
 };
