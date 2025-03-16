9f4c95d30d84515d89891a52ac969276e333ebab
MesachVenegas <mesach.venegas@hotmail.com>
2025-03-16 14:27:49 -0700

    add: logout clear cookies

--- backend/src/application/auth/use-cases/user-logout.ts
+++ backend/src/application/auth/use-cases/user-logout.ts
@@ -1,12 +1,19 @@
 import { TokenVersionRepository } from '@/domain/interfaces/token-version-repository';
+import { Response } from 'express';
 
 export class LogOutUserUseCase {
   constructor(
     private readonly tokenVersionRepository: TokenVersionRepository,
   ) {}
 
-  async execute(id: string) {
+  async execute(id: string, res: Response) {
     const token = await this.tokenVersionRepository.invalidateVersion(id);
+
+    res.cookie('_ngx_access_token', '', {
+      maxAge: 0,
+    });
+
+    res.cookie('__ngx_csrf__', '', { maxAge: 0 });
 
     return token;
   }
