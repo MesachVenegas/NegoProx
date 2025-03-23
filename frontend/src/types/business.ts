efccf5b9f6f85f07a927b8e5a26b71eabda255a5
MesachVenegas <mesach.venegas@hotmail.com>
2025-03-23 15:44:29 -0700

    feat(types): add BusinessProfile and BusinessCard types
    Introduce new types to support business profile and card functionality. This includes adding a `slug` field to `BusinessProps` for better URL handling and defining `BusinessProfile` to manage social media and other profile-related data.

--- frontend/src/types/business.ts
+++ frontend/src/types/business.ts
@@ -6,8 +6,25 @@ export interface ServiceProps {
 	date: string;
 }
 
+export type BusinessProfile = {
+	id: string;
+	bannerImage: string;
+	website: string;
+	socialMedia: {
+		facebook?: string;
+		instagram?: string;
+		twitter?: string;
+		youtube?: string;
+		linkedin?: string;
+		tiktok?: string;
+	};
+	createdAt: Date;
+	updatedAt: Date;
+};
+
 export type BusinessProps = {
 	id: string;
+	slug: string;
 	name: string;
 	description: string;
 	address: string;
@@ -17,3 +34,17 @@ export type BusinessProps = {
 	createdAt: Date;
 	updatedAt: Date;
 };
+
+export interface BusinessCard extends BusinessProps {
+	businessProfile: BusinessProfile;
+	categories: {
+		categoryId: string;
+		category: {
+			id: string;
+			name: string;
+			en_name: string;
+			svg_icon: string;
+		};
+	}[];
+	rateAvg: number;
+}
