2522c69be6e2a62cdddeebf113db13dceb5dc9c2
MesachVenegas <mesach.venegas@hotmail.com>
2025-04-03 20:12:42 -0700

    feat(utils): add utility functions for time, day, and price formatting
    Add new utility functions `formatTime`, `getDayName`, and `formatPrice` to handle time formatting, day name retrieval based on locale, and price formatting in Mexican pesos. These functions enhance the frontend's ability to display localized and formatted data.

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,44 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function formatTime(timeString: Date) {
+	const date = new Date(timeString);
+	return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+}
+
+export function getDayName(dayOfWeek: number, locale: string) {
+	const enDays = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	const esDays = [
+		"Domingo",
+		"Lunes",
+		"Martes",
+		"Miércoles",
+		"Jueves",
+		"Viernes",
+		"Sábado",
+	];
+
+	if (locale === "en") return enDays[dayOfWeek];
+	if (locale === "es") return esDays[dayOfWeek];
+
+	return "";
+}
+
+export function formatPrice(price: number) {
+	return new Intl.NumberFormat("es-Mx", {
+		style: "currency",
+		currency: "MXN",
+	}).format(price);
 }
