e48431b1930bfa758d47aeacbb35e97b84e1c87d
MesachVenegas <mesach.venegas@hotmail.com>
2025-04-05 14:50:39 -0700

    feat(review-repository): add CRUD operations to review repository interface
    This commit introduces new methods to the `ReviewRepository` interface to support CRUD operations. The added methods include `getReviewById`, `createReview`, `updateReview`, and `deleteReview`, enabling full management of review entities within the system.

--- backend/src/domain/interfaces/review-repository.ts
+++ backend/src/domain/interfaces/review-repository.ts
@@ -2,6 +2,7 @@ import { Review } from '../entities';
 
 export interface ReviewRepository {
   countReviews(id: string): Promise<number>;
+  getReviewById(id: string): Promise<Review | null>;
   getReviews(
     id: string,
     sortBy: string,
@@ -10,4 +11,7 @@ export interface ReviewRepository {
     order: 'asc' | 'desc',
   ): Promise<Review[]>;
   getAverageRate(id: string): Promise<number>;
+  createReview(review: Review): Promise<Review>;
+  updateReview(review: Review): Promise<Review>;
+  deleteReview(id: string): Promise<Review>;
 }
